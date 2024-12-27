7ff9fb9e244fa03dccd9d4e3abcd36a097f566be
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-12-27 23:37:52 +0530

    make a navigation component with layout in it

--- app/page.js
+++ app/page.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import Navigation from "./components/navigation";
+
 export default function Page() {
-  return <h1>The Wild Oasis. Welcome to Paradise</h1>;
+  return (
+    <div>
+      {/* <Navigation /> */}
+      <h1>The Wild Oasis. Welcome to Paradise</h1>
+
+      {/*
+        This will cause whole page to reload      
+      <a href="/cabins">explore luxury cabin</a>*/}
+      <Link href="/cabins">Explore Luxury Cabins</Link>
+    </div>
+  );
 }
