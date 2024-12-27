92fa2e22b305badc0ff13805034b6920739fa5c6
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-12-28 00:15:04 +0530

    make the simple loading component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,21 @@
+import Counter from "../components/Counter";
 import Navigation from "../components/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  // console.log(data);
+
   return (
     <div>
       {/* <Navigation /> */}
       <h1>Cabins page</h1>
+      <ul>
+        {data.map((el) => (
+          <li key={el.id}>{el.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
     </div>
   );
 }
---
+++ app/loading.js
@@ -0,0 +1,4 @@
+// we just have to make this file with loading.js name and it will return  the loading spinner or anything that we defin
+export default function Loading() {
+  return <p>Loading Data . . . . . </p>;
+}
